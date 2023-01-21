52936cd3d9b163bd25d6954b2bf84beff7827af1
MBouziyani <mb.bouziyani@gmail.com>
2023-01-21 23:08:22 +0100

    application e-commerce

---
+++ Application-web-e-commerce/includes/templates/footer.php
@@ -0,0 +1,8 @@
+
+
+    <footer class="jumbotron text-center mt-auto bg-dark text-white">2022 Made by : Wishipping</footer>
+    <script src='<?php echo $js; ?>jquery-3.5.1.js'></script>
+    <script src='<?php echo $js; ?>bootstrap.min.js'></script>
+    <script src='<?php echo $js; ?>frontend.js'></script>
+</body>
+</html>
